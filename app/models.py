33bf68923dc8357499be17bdb21c279492cfa062
LeewelKarani <Leewelkarani@gmail.com>
2018-12-07 09:58:22 +0300

    Add private Message Model

--- app/models.py
+++ app/models.py
@@ -72,6 +72,13 @@ class User(UserMixin, db.Model):
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    messages_sent = db.relationship('Message',
+                                      foreign_keys='Message.sender_id',
+                                      backref='author', lazy='dynamic')
+    messages_received = db.relationship('Message',
+                                          foreign_keys='Message.recipient_id',
+                                          backref='recipient', lazy='dynamic')
+    last_message_read_time = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -122,9 +129,14 @@ class User(UserMixin, db.Model):
         return User.query.get(id)
 
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+    @login.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(
+           Message.timestamp > last_read_time).count()
 
 
 class Post(SearchableMixin, db.Model):
@@ -137,3 +149,14 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+class Message(db.Model):
+      id = db.Column(db.Integer, primary_key=True)
+      sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+      recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+      body = db.Column(db.String(140))
+      timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+      def __repr__(self):
+          return '<Message {}>'.format(self.body)
