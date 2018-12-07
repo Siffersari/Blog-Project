155ccf3e9dd1aa65c51a5b26a5cba2675eb86c05
LeewelKarani <Leewelkarani@gmail.com>
2018-12-07 08:22:43 +0300

    Add popup

--- app/main/routes.py
+++ app/main/routes.py
@@ -77,6 +77,13 @@ def user(username):
                            next_url=next_url, prev_url=prev_url)
 
 
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user_popup.html', user=user)
+
+
 @bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
@@ -147,4 +154,4 @@ def search():
     prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
         if page > 1 else None
     return render_template('search.html', title=_('Search'), posts=posts,
-                           next_url=next_url, prev_url=prev_url)
+           next_url=next_url, prev_url=prev_url)
